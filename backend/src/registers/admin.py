577c169a6b13ac7051c111c0409562206a2c291d
Vasily Toropchin <vasview@gmail.com>
2023-07-30 22:49:14 +0600

    Added JobPostion, AcademicTitle, AcademicName to register.

--- backend/src/registers/admin.py
+++ backend/src/registers/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import AcademicDegree, AcademicTitle, ResearchDataBase
+from .models import *
 
 admin.site.register(AcademicDegree)
 admin.site.register(AcademicTitle)
 admin.site.register(ResearchDataBase)
+admin.site.register(JobPosition)
+admin.site.register(AcademicYear)
+admin.site.register(FundSource)
--- backend/src/registers/models.py
+++ backend/src/registers/models.py
@@ -40,3 +40,43 @@ class ResearchDataBase(models.Model):
     def __str__(self):
         return self.name
     
+class FundSource(models.Model):
+    code = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        verbose_name = 'Справочник фондов'
+        verbose_name_plural = 'Справочник фондов'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class JobPosition(models.Model):
+    code = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        verbose_name = 'Справочник должности'
+        verbose_name_plural = 'Список должностей'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name    
+    
+class AcademicYear(models.Model):
+    name                    = models.CharField(max_length=255, blank=True, null=True)
+    first_semester_start    = models.DateField()
+    first_semester_end      = models.DateField()
+    second_semester_start   = models.DateField()
+    second_semester_end     = models.DateField()
+    active                  = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Учебный год'
+        verbose_name_plural = 'Справочник учебных годов'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name    
+    
