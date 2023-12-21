317891bd233979426b1f37e8c59ce6bb9c3ef222
Vasily Toropchin <vasview@gmail.com>
2023-12-21 22:59:43 +0600

    Updated routes with pages.

--- webapp/src/routes/index.jsx
+++ webapp/src/routes/index.jsx
@@ -5,8 +5,10 @@ import Home from '../pages/Home';
 import RequireAuth from '../features/auth/RequireAuth';
 import Login from '../pages/Login';
 import MyProfile from '../pages/MyProfile/MyProfile';
+import MyProfileEdit from '../pages/MyProfile/MyProfileEdit';
 import Employees from '../pages/Employees/Employees';
 import ScientificProfile from '../pages/ScientificProfile/ScientificProfile';
+import ScientificProfileEdit from '../pages/ScientificProfile/ScientificProfileEdit';
 import MyGestLectures from '../pages/MyGestLectures';
 import MyConferences from '../pages/MyConferences';
 import MyPatents from '../pages/MyPatents';
@@ -14,6 +16,7 @@ import MyInventions from '../pages/MyInventions';
 import MyCopyrightCertificates from '../pages/MyCopyrightCertificates';
 import MyMonographPublications from '../pages/MyMonographPublications';
 import MyResearchPublications from '../pages/MyResearchPublications';
+import MyGScholarPublications from '../pages/MyGScholarPublications';
 import MyDissertations from '../pages/MyDissertations';
 import MyPostgraduates from '../pages/MyPostgraduates';
 import MyResearchManagement from '../pages/MyResearchManagement';
@@ -25,13 +28,12 @@ import EmployeeCard from '../pages/EmployeeCard/EmployeeCard'
 import ResetPassword from '../pages/ResetPassword';
 import ResetPasswordConfirm from '../pages/ResetPasswordConfirm';
 
-import history from '../_helpers/history';
 import NotFound from '../pages/NotFound';
 
 
 const RouteTable = () => {
   return (
-    <Router history={history}>
+    <Router>
       <Routes>
         <Route path='/' element={<BaseLayout />}>
           {/* public routes */}
@@ -45,7 +47,9 @@ const RouteTable = () => {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="employees_card" element={<EmployeeCard />} />
             <Route path="my_profile" element={<MyProfile />} />
+            <Route path="edit_profile/:id" element={<MyProfileEdit />} />
             <Route path="my_sci_profile" element={<ScientificProfile />} />
+            <Route path="my_sci_profile/:id" element={<ScientificProfileEdit />} />
             <Route path="my_employees" element={<Employees />} />
             <Route path="my_guest_lectures" element={<MyGestLectures />} />
             <Route path="my_conferences" element={<MyConferences />} />
@@ -54,6 +58,7 @@ const RouteTable = () => {
             <Route path="my_copyright_certificates" element={<MyCopyrightCertificates />} />
             <Route path="my_monograph_publications" element={<MyMonographPublications />} />
             <Route path="my_research_publications" element={<MyResearchPublications />} />
+            <Route path="my_gscholar_publications" element={<MyGScholarPublications />} />
             <Route path="my_dissertations" element={<MyDissertations />} />
             <Route path="my_postgraduates" element={<MyPostgraduates />} />
             <Route path="my_resarch_management" element={<MyResearchManagement />} />
