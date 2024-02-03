6fb79c26affbeefd85a567eb5c0f80bbafaa9142
Vasily Toropchin <vasview@gmail.com>
2024-02-03 22:13:57 +0600

    Added routes to the cards.

--- webapp/src/pages/Dashboard/Dashboard.jsx
+++ webapp/src/pages/Dashboard/Dashboard.jsx
@@ -8,16 +8,16 @@ import { useGetUserQuery } from '../../features/auth/userApiSlice';
 export const Dashboard = () => {
   const cards = [
     { id: 1, title: 'Гостевые лекции и стажировки', number: 1, link: '/my_guest_lectures'},
-    { id: 2, title: 'Семинары и конференции', number: 3, link: ''},
-    { id: 3, title: 'Рук-во докторантами, аспирантов', number: 2, link: ''},
-    { id: 4, title: 'Защита диссертаций', number: 1, link: ''},
-    { id: 5, title: 'Руководство НИРС', number: 5, link: ''},
-    { id: 6, title: 'Научно исследовательская работа', number: 1, link: ''},
-    { id: 7, title: 'Публикация по итокам НИРС', number: 1, link: ''},
-    { id: 8, title: 'Публикация монографий', number: 1, link: ''},
-    { id: 9, title: 'Получено авторских свидетельств', number: 2, link: ''},
-    { id: 10, title: 'Заявки на изобретения', number: 9, link: ''},
-    { id: 11, title: 'Патенты на изобретения', number: 3, link: ''},
+    { id: 2, title: 'Семинары и конференции', number: 3, link: '/my_conferences'},
+    { id: 3, title: 'Рук-во докторантами, аспирантов', number: 2, link: '/my_postgraduates'},
+    { id: 4, title: 'Защита диссертаций', number: 1, link: '/my_dissertations'},
+    { id: 5, title: 'Руководство НИРС', number: 5, link: '/my_resarch_management'},
+    { id: 6, title: 'Научно исследовательская работа', number: 1, link: '/my_resarch_works'},
+    { id: 7, title: 'Публикация по итокам НИРС', number: 1, link: '/my_research_publications'},
+    { id: 8, title: 'Публикация монографий', number: 1, link: '/my_monograph_publications'},
+    { id: 9, title: 'Получено авторских свидетельств', number: 2, link: '/my_copyright_certificates'},
+    { id: 10, title: 'Заявки на изобретения', number: 9, link: '/my_inventions'},
+    { id: 11, title: 'Патенты на изобретения', number: 3, link: '/my_patents'},
   ]
 
   const dispatch = useDispatch();
