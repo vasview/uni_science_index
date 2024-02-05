dd148c720dc522388dece1b01d6d79b0e3b83024
Vasily Toropchin <vasview@gmail.com>
2024-02-06 04:31:08 +0600

    Updated settings file.

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -15,6 +15,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-scifi-test2424')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
+CSRF_TRUSTED_ORIGINS = ['https://sc.kstu.kg', 'http://sc.kstu.kg']
+
 ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
