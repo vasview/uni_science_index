3b06fe7d5e0643332cf6427bbaad7ede5a3e1aca
Vasily Toropchin <vasview@gmail.com>
2024-02-25 09:26:24 +0600

    Added display fields for ResearchDataBases in Admin panel.

--- backend/src/registers/admin.py
+++ backend/src/registers/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+class ResearchDataBaseAdmin(admin.ModelAdmin):
+  list_display = ['name', 'url', 'request_param', 'sort', 'active']
+
 admin.site.register(AcademicDegree)
 admin.site.register(AcademicTitle)
-admin.site.register(ResearchDataBase)
+admin.site.register(ResearchDataBase, ResearchDataBaseAdmin)
 admin.site.register(JobPosition)
 admin.site.register(AcademicYear)
 admin.site.register(FundSource)
