a0e78db0c362db0eb6bf673d22296c70de48f4da
Vasily Toropchin <vasview@gmail.com>
2024-02-25 09:22:24 +0600

    Provided plural name for the Publication application.

--- backend/src/publications/apps.py
+++ backend/src/publications/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PublicationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'publications'
+    verbose_name = 'Публикации'
